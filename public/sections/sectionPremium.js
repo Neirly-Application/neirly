074e52bb61acfd8d5a45ab93e2daeaccd3575983
Silenziosoh <lorenzolorigaa08@gmail.com>
2025-07-21 23:58:11 +0200

    Small Changes

--- public/sections/sectionPremium.js
+++ public/sections/sectionPremium.js
@@ -44,6 +44,12 @@ export default async function loadPremiumSection(content, user) {
           <div class="premium-user-card-info">
             <h2>${user.nickname || user.name || 'User'}</h2>
             <p><span>Biography:</span>${user.about_me || '<i>No biography</i>'}</p>
+            <div class="premium-fancy-line-2"></div>
+            <p><strong>One year of neirly?</strong></p>
+            <p>Redeem this card now, and it'll be delivered <strong>straight</strong> to your door!<br>
+                What's on offer?<br>
+                Here you'll find the new offers and surprises perfect for you!<br>
+                You can get a vacation, cash, things you love, and much more!</p>
           </div>
         </div>
       </div>
